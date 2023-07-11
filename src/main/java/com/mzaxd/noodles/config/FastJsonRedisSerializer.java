77c0eb6baa0050d79aac993c2b5399d36f4e7cd1
caihaohan <caihaohan0712@foxmail.com>
2023-07-11 14:37:18 +0800

    ci: 常量优化

--- src/main/java/com/mzaxd/noodles/config/FastJsonRedisSerializer.java
+++ src/main/java/com/mzaxd/noodles/config/FastJsonRedisSerializer.java
@@ -9,6 +9,7 @@ import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.SerializationException;
 
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Redis使用FastJson序列化
@@ -17,9 +18,9 @@ import java.nio.charset.Charset;
  */
 public class FastJsonRedisSerializer<T> implements RedisSerializer<T> {
 
-    public static final Charset DEFAULT_CHARSET = Charset.forName("UTF-8");
+    public static final Charset DEFAULT_CHARSET = StandardCharsets.UTF_8;
 
-    private Class<T> clazz;
+    private final Class<T> clazz;
 
     static {
         ParserConfig.getGlobalInstance().setAutoTypeSupport(true);
